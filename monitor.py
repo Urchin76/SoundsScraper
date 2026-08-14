e68fc8caf06165968d035eeed97408e68c393019
Urchin76 <urchin76@hotmail.com>
2026-08-14 03:33:27 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -16,11 +16,9 @@ DB_NAME = "sounds_catalog.db"
 
 # Headers die exact een echte Chrome browser op Windows simuleren
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "nl-NL,nl;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Cache-Control": "no-cache",
-    "Pragma": "no-cache"
 }
 
 # --- E-MAIL FUNCTIE ---
